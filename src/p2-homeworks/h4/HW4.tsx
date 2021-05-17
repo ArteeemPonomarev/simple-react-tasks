60eb7f5ccf0e62853ef7a2bc79ca1c944d383217
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-05-17 23:37:02 +0300

    4, Button classes

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -25,13 +25,15 @@ function HW4() {
             homeworks 4
 
             <div className={s.column}>
+
+
                 <SuperInputText
                     value={text}
                     onChangeText={setText}
                     onEnter={showAlert}
                     error={error}
-                    spanClassName={s.testSpanError}
-                />
+                    spanClassName={s.testSpanError}/>
+
 
                 <SuperInputText
                     className={s.blue} // проверьте, рабоет ли смешивание классов
@@ -50,7 +52,7 @@ function HW4() {
                     delete {/*// название кнопки попадёт в children*/}
                 </SuperButton>
 
-                <SuperButton disabled>
+                <SuperButton disabled onClick={showAlert}>
                     disabled
                 </SuperButton>
 
